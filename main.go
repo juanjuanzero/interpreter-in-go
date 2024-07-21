be7fb44aa8f1352393bdaf584bb1adc5b71a3710
juanjuanzero <juan.afable@flocksafety.com>
2024-07-21 06:35:05 -0400

    closing out chapter 1

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/juanjuanzero/monkey/repl"
+)
 
 func main() {
-	fmt.Println("Welcome!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the monkey language! \n", user.Username)
+	fmt.Printf("Type the commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/juanjuanzero/monkey/lexer"
+	"github.com/juanjuanzero/monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			// get out
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
